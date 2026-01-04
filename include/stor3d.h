088ae557851c2cf0795259fadf16e3a77453bc44
Yunho Park <yunhpark@c1r21s3.42gyeongsan.kr>
2026-01-04 14:51:39 +0900

    init : 기본 뼈대 생성 및 저장소 생성

---
+++ include/stor3d.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stor3d.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yunhpark <yunhpark@student.42gyeongsan.kr> +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/04 13:06:14 by yunhpark          #+#    #+#             */
+/*   Updated: 2026/01/04 14:41:34 by yunhpark         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef STOR3D_H
+# define STOR3D_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <string.h>
+# include <ctype.h>
+# include <errno.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include<sys/types.h>
+# include<sys/stat.h>
+
+#define BLOCK_SIZE 4096
+#define BLOCK_COUNT 8192
+
+
+#endif
