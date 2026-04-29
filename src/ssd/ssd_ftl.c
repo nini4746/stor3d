87bd00abc2303e976367610b5edfc0ab6e531725
nini4746 <nini4746@github.com>
2026-04-29 10:49:03 +0900

    fix: ssd_allocate_page wrap-around 시 valid 페이지 덮어쓰기 버그 수정

--- src/ssd/ssd_ftl.c
+++ src/ssd/ssd_ftl.c
@@ -22,6 +22,29 @@ int	ssd_get_page_index(size_t ppa)
 	return (ppa % SSD_PAGES_PER_BLOCK);
 }
 
+static size_t	ssd_find_free_ppa(t_ssd_state *ssd, size_t start)
+{
+	size_t	scanned;
+	size_t	ppa;
+	int		bi;
+	int		pi;
+
+	scanned = 0;
+	ppa = start;
+	while (scanned < SSD_TOTAL_PAGES)
+	{
+		bi = ssd_get_block_index(ppa);
+		pi = ssd_get_page_index(ppa);
+		if (ssd->blocks[bi].pages[pi] == SSD_PAGE_FREE)
+			return (ppa);
+		ppa++;
+		if (ppa >= SSD_TOTAL_PAGES)
+			ppa = 0;
+		scanned++;
+	}
+	return ((size_t) - 1);
+}
+
 size_t	ssd_allocate_page(t_ssd_state *ssd)
 {
 	size_t	ppa;
@@ -30,13 +53,15 @@ size_t	ssd_allocate_page(t_ssd_state *ssd)
 
 	if (ssd->free_pages == 0)
 		return ((size_t) - 1);
-	ppa = ssd->next_free_ppa;
+	ppa = ssd_find_free_ppa(ssd, ssd->next_free_ppa);
+	if (ppa == (size_t) - 1)
+		return ((size_t) - 1);
 	block_idx = ssd_get_block_index(ppa);
 	page_idx = ssd_get_page_index(ppa);
 	ssd->blocks[block_idx].pages[page_idx] = SSD_PAGE_VALID;
 	ssd->blocks[block_idx].valid_count++;
 	ssd->free_pages--;
-	ssd->next_free_ppa++;
+	ssd->next_free_ppa = ppa + 1;
 	if (ssd->next_free_ppa >= SSD_TOTAL_PAGES)
 		ssd->next_free_ppa = 0;
 	return (ppa);
